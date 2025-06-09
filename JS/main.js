2beef9c25b6c3614cbd2b90711c6dec03c9fdd04
Nickkklll <98213520+Nickkklll@users.noreply.github.com>
2025-06-09 19:01:16 +0300

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,25 +1,27 @@
-let board = document.querySelector("#board");
-let score = document.querySelector("#atemptNumOutput");
-let startGameButton = document.querySelector("#startGame");
- alert(`Завдання: перегортати картки та знаходити пари.
+window.onload = function() {
+  let board = document.querySelector("#board");
+  let score = document.querySelector("#atemptNumOutput");
+  let startGameButton = document.querySelector("#startGame");
+  alert(`Завдання: перегортати картки та знаходити пари.
   Якщо ти пройшов цей тест, мої ВІТАННЯ ти справжній гей!
   Тицьни "ОК"`);
-let gm = new GameManager(board, score);
-gm.startGame();
-
-board.addEventListener("click", function (e) {
-  let clickedCard = e.target.connectedCard;
-  if (clickedCard) {
-    gm.selectCard(clickedCard);
-  }
-});
-startGameButton.addEventListener("click", function () {
-  startGameButton.disabled = true;
+  let gm = new GameManager(board, score);
   gm.startGame();
-  setTimeout(() => {
-    startGameButton.disabled = false; // разблокируем через 2 секунды
-  }, 1500);
-});
+
+  board.addEventListener("click", function (e) {
+    let clickedCard = e.target.connectedCard;
+    if (clickedCard) {
+      gm.selectCard(clickedCard);
+    }
+  });
+  startGameButton.addEventListener("click", function () {
+    startGameButton.disabled = true;
+    gm.startGame();
+    setTimeout(() => {
+      startGameButton.disabled = false;
+    }, 1500);
+  });
+};
 
 
 
